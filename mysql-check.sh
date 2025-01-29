9aa590d98614cc6c92f26ba2347ef0952eff8463
Virupakshi047 <144578836+Virupakshi047@users.noreply.github.com>
2025-01-29 23:04:30 +0530

    Update mysql-check.sh

--- mysql-check.sh
+++ mysql-check.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# Update package list
 echo "Updating package list..."
 sudo apt-get update -y
-
-# Install MySQL server
 echo "Installing MySQL server..."
 sudo apt-get install -y mysql-server
-
-# Start MySQL service if it's not already running
 echo "Starting MySQL service..."
 sudo systemctl start mysql
 
-# Enable MySQL service to start on boot
+
 echo "Enabling MySQL to start on boot..."
 sudo systemctl enable mysql
 
-# Check MySQL server status
+
 echo "Checking MySQL server status..."
 mysql_status=$(sudo systemctl status mysql | grep "Active:")
 
